79142ceff9b0c9e34f551284a3f24afa90f9aec0
LuChaopeng <chaopeng.lu@qq.com>
2021-11-18 00:42:39 +0800

    接收时光卡片

--- mt.js
+++ mt.js
@@ -4,14 +4,21 @@ const koaBodyParser = require('koa-bodyparser')
 const app = new Koa()
 app.use(koaBodyParser())
 app.use(async (ctx,next)=>{
-    if('req' in ctx.request.body && ctx.request.body.req === 'init'){
-        console.log('检查cookie，控制访问页面')
-        ctx.body = ctx.cookies.get('userName') === undefined ? 'select-user' : ctx.cookies.get('userName')
-    }
-    if ('userName' in ctx.request.body){
-        ctx.cookies.set('userName', ctx.request.body.userName)
-        ctx.body = 'success'
-        await next()
+    switch (ctx.url){
+        case '/':{
+            if('req' in ctx.request.body && ctx.request.body.req === 'init'){
+                console.log('检查cookie，控制访问页面')
+                ctx.body = ctx.cookies.get('userName') === undefined ? 'select-user' : ctx.cookies.get('userName')
+            }
+            if ('userName' in ctx.request.body){
+                ctx.cookies.set('userName', ctx.request.body.userName)
+                ctx.body = 'success'
+            }
+        }
+        case '/tag-controller':{
+            console.log(ctx.request.body)
+            ctx.body = 'success'
+        }
     }
 })
 
